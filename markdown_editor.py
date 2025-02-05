26ff4f227e11f229f0066f26e01d4bc96d3721ca
OmarTebo <eltabarany.omar@gmail.com>
2025-02-05 03:04:38 +0200

    Implemented Edit menu front and back (Copy, Cut, Paste, Undo, Redo, Select All)

--- markdown_editor.py
+++ markdown_editor.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget,
                              QTextEdit, QHBoxLayout, QMenuBar,
                              QMenu, QStatusBar, QFileDialog, QMessageBox)
 from PyQt6 import QtCore
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction, QIcon
 import sys
 
 class MarkdownEditor(QMainWindow):
@@ -11,7 +11,8 @@ class MarkdownEditor(QMainWindow):
         
         self.window_width, self.window_height = 700, 500
         self.setMinimumSize(self.window_width, self.window_height)
-        self.setWindowTitle("Untitled - My Journal App")
+        self.setWindowIcon(QIcon("thoughForge.ico"))
+        self.setWindowTitle("Untitled - ThoughtForge")
         self.setStyleSheet("""
                             QWidget {
                                 font-size: 18px;
@@ -89,21 +90,62 @@ class MarkdownEditor(QMainWindow):
         self.actionEditor_Only = QAction(self)
         self.actionEditor_Only.setText("Editor Only")
         self.actionEditor_Only.setStatusTip("View Editor Only")
+        self.actionEditor_Only.setShortcut("Ctrl+1")
         # View -> Viewer Only
         self.actionViewer_Only = QAction(self)
         self.actionViewer_Only.setText("Viewer Only")
         self.actionViewer_Only.setStatusTip("Viewer Only")
+        self.actionViewer_Only.setShortcut("Ctrl+2")
         # View -> Split View
         self.actionSplit_View = QAction(self)
         self.actionSplit_View.setText("Split View")
         self.actionSplit_View.setStatusTip("Vertical Split Between Editor and Viewer")
+        self.actionSplit_View.setShortcut("Ctrl+3")
         # Adding Actions to View menu
         self.menuView.addAction(self.actionEditor_Only)
         self.menuView.addAction(self.actionViewer_Only)
         self.menuView.addAction(self.actionSplit_View)
         
+        # Edit menu in menu bar
+        self.menuEdit = QMenu(self.menu_bar)
+        self.menuEdit.setTitle("Edit")
+        # Edit -> Undo
+        self.actionUndo = QAction(self)
+        self.actionUndo.setText("Undo")
+        self.actionUndo.setShortcut("Ctrl+Z")
+        # Edit -> Redo
+        self.actionRedo = QAction(self)
+        self.actionRedo.setText("Redo")
+        self.actionRedo.setShortcut("Ctrl+Shift+Z")
+        # Edit -> Cut
+        self.actionCut = QAction(self)
+        self.actionCut.setText("Cut")
+        self.actionCut.setShortcut("Ctrl+X")
+        # Edit -> Copy
+        self.actionCopy = QAction(self)
+        self.actionCopy.setText("Copy")
+        self.actionCopy.setShortcut("Ctrl+C")
+        # Edit -> Paste
+        self.actionPaste = QAction(self)
+        self.actionPaste.setText("Paste")
+        self.actionPaste.setShortcut("Ctrl+V")
+        # Edit -> Select All
+        self.actionSelectAll = QAction(self)
+        self.actionSelectAll.setText("Select All")
+        self.actionSelectAll.setShortcut("Ctrl+A")
+        # Adding Actions to Edit menu
+        self.menuEdit.addAction(self.actionUndo)
+        self.menuEdit.addAction(self.actionRedo)
+        self.menuEdit.addSeparator()
+        self.menuEdit.addAction(self.actionCut)
+        self.menuEdit.addAction(self.actionCopy)
+        self.menuEdit.addAction(self.actionPaste)
+        self.menuEdit.addSeparator()
+        self.menuEdit.addAction(self.actionSelectAll)
+        
         # Adding menus to the menu bar
         self.menu_bar.addAction(self.menuFile.menuAction())
+        self.menu_bar.addAction(self.menuEdit.menuAction())
         self.menu_bar.addAction(self.menuView.menuAction())
         
         
@@ -114,6 +156,13 @@ class MarkdownEditor(QMainWindow):
         self.actionSave.triggered.connect(self.save_file)
         self.actionSave_As.triggered.connect(self.save_as_file)
         self.actionOpen.triggered.connect(self.open_file)
+        
+        self.actionUndo.triggered.connect(self.md_editor.undo)
+        self.actionRedo.triggered.connect(self.md_editor.redo)
+        self.actionCut.triggered.connect(self.md_editor.cut)
+        self.actionCopy.triggered.connect(self.md_editor.copy)
+        self.actionPaste.triggered.connect(self.md_editor.paste)
+        self.actionSelectAll.triggered.connect(self.md_editor.selectAll)
         
         self.actionEditor_Only.triggered.connect(self.show_editor_only)
         self.actionViewer_Only.triggered.connect(self.show_viewer_only)
@@ -129,7 +178,7 @@ class MarkdownEditor(QMainWindow):
         
         # Reset the file path and window title
         self.current_file_path = None
-        self.setWindowTitle("Untitled - My Journal App")
+        self.setWindowTitle("Untitled - ThoughtForge")
         
     def save_file(self):
         if self.current_file_path:
@@ -165,7 +214,7 @@ class MarkdownEditor(QMainWindow):
                 
                 # Update the current file path and window title
                 self.current_file_path = fileName
-                self.setWindowTitle(f"{fileName} - My Journal App")
+                self.setWindowTitle(f"{fileName} - ThoughtForge")
                 self.statusbar.showMessage(f"File saved successfully: {fileName}", 3000)
             except Exception as e:
                 QMessageBox.critical(self, "Error", f"Failed to save file: {str(e)}")
@@ -190,7 +239,7 @@ class MarkdownEditor(QMainWindow):
                 
                 # Update the current file path and window title
                 self.current_file_path = fileName
-                self.setWindowTitle(f"{fileName} - My Journal App")
+                self.setWindowTitle(f"{fileName} - ThoughtForge")
                 self.statusbar.showMessage(f"File opened: {fileName}", 3000)
             except Exception as e:
                 QMessageBox.critical(self, "Error", f"Failed to open file: {str(e)}")
